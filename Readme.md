36f0c0cc4b2e73f0c4b7cff921c260c167d7077c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to make GroupBox look as GroupFrame
+
+
+This example demonstrates how to make GroupBox look as GroupFrame.
+
+<br/>
+
+
---
+++ VB/GroupFrameWithinGroupBox/My Project/AssemblyInfo.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+Imports System.Windows
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("GroupFrameWithinGroupBox")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("Microsoft")>
+<Assembly: AssemblyProduct("GroupFrameWithinGroupBox")>
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2011")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+#If SILVERLIGHT Then
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d82e0d99-764c-459b-95e5-39c0cd182a70")>
+<Assembly: AssemblyVersion("1.0.0.0"), assembly: AssemblyFileVersion("1.0.0.0")>
+#Else
+<Assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly), assembly: AssemblyVersion("1.0.0.0"), assembly: AssemblyFileVersion("1.0.0.0")> 'where the generic resource dictionary is located - where theme specific resource dictionaries are located
+    '(used if a resource is not found in the page, 
+    ' or application resource dictionaries)
+    '(used if a resource is not found in the page, 
+    ' app, or any theme specific resource dictionaries)
+#End If
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Revision and Build Numbers 
+' by using the '*' as shown below:
